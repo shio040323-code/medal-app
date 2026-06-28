365d1d56e271c78fc3f89f9686ea60fd244f3b63
shio040323-code <shio040323@gmail.com>
2026-06-28 23:16:27 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,74 @@
-const CACHE_NAME = "medal-app-v1"
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/data.json"
-]
+const CACHE_NAME = "medal-app-v4";
 
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./data.json",
+  "./noimage.png"
+];
+
+// インストール
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  )
-})
+  );
 
+  self.skipWaiting();
+});
+
+// 有効化
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// 通信
 self.addEventListener("fetch", event => {
+
+  // imagesフォルダは取得したら保存
+  if (event.request.url.includes("/images/")) {
+
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache =>
+        cache.match(event.request).then(response => {
+
+          return response || fetch(event.request)
+            .then(networkResponse => {
+
+              cache.put(
+                event.request,
+                networkResponse.clone()
+              );
+
+              return networkResponse;
+            });
+        })
+      )
+    );
+
+    return;
+  }
+
+  // HTML・JSON
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  )
-})
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+  );
+
+});
